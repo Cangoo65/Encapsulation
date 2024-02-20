cf43203eb1efe3a3902cd3e70ac905a8c0a1ed48
Cangoo65 <caneraytekin06@hotmail.com>
2024-02-20 20:54:42 +0300

    Proje dosyası ekle.

---
+++ Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    internal class Person
+    {
+        private int age = 18;
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value > 0)
+                    age = value;
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using Encapsulation;
+
+Person person = new();
+person.Age = -1;
+Console.WriteLine(person.Age);
+Console.Read();
